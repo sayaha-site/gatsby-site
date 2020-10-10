59878ea11134ad59d04614bb473359eb7f6ea106
sayaha-site <72655768+sayaha-site@users.noreply.github.com>
2020-10-11 00:26:04 +0900

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,10 +6,10 @@ export default function Info() {
   return (
     <Layout page="about">
       <section className={infoStyles.info_blurb}>
-          高橋沙也葉 Sayaha TAKAHASHI  
-    　      
-          1995年三重県生まれ。戦後日本とアメリカのアートシーンの交差に関心を持ち研究を行っている。  
-          2020年現在は京都大学大学院に在学し、とりわけ1960年代から70年代におけるアメリカ美術の「脱物質化」の傾向について、  
+          高橋沙也葉 Sayaha TAKAHASHI<br>
+    　    <br>
+          1995年三重県生まれ。戦後日本とアメリカのアートシーンの交差に関心を持ち研究を行っている。<br>
+          2020年現在は京都大学大学院に在学し、とりわけ1960年代から70年代におけるアメリカ美術の「脱物質化」の傾向について、<br>
           リチャード・セラ等の作家に着目して研究を行っている。
       </section>
     </Layout>
