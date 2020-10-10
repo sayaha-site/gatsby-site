9508a5b2c96dd3502599b87af37829215b7fa858
sayaha-site <72655768+sayaha-site@users.noreply.github.com>
2020-10-10 22:12:50 +0900

    Update BlogList.js

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -16,7 +16,6 @@ export default function BlogList() {
                 <li className={blogListStyles.li} key={blog.node.fields.slug}>
                   <div className={blogListStyles.list__info}>
                     <h2>{blog.node.frontmatter.title}</h2>
-                    <h3>{blog.node.frontmatter.date}</h3>
                     <p>{blog.node.excerpt}</p>
                   </div>
                 </li>
