48c87f0936ce5ff8ce43e7e2c66429e31a5b1cfe
sayaha-site <72655768+sayaha-site@users.noreply.github.com>
2021-01-21 19:38:18 +0900

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,18 +7,18 @@ export default function Info() {
     <Layout page="about">
       <section className={infoStyles.info_blurb}>
           高橋沙也葉 Sayaha TAKAHASHI<br/>
-    　    <br/>
           <br/>
-          2015年 筑波大学 芸術専門学群 芸術学専攻　入学<br/>
+          2015年 筑波大学 芸術専門学群 芸術学専攻 入学<br/>
           2017年 カリフォルニア州立大学 ロングビーチ校　美術史専攻 交換留学<br/>
           2020年 筑波大学 芸術専門学群 芸術学専攻　芸術支援コース 卒業<br/>
           2021年 京都大学大学院 人間環境学研究科 思想文化論講座 修士課程 在学<br/>
           <br/>
           活動歴抜粋<br/>
-　　　　　 2015年 「Don't follow the Wind」展 学生スタッフ<br/>
+          2015年 「Don't follow the Wind」展 学生スタッフ<br/>
           2016年 スイス大使館主催「ダダイズム100周年」ライター<br/>
           2016年 ギャラリー SNOW Contemporary インターンシップ<br/>
           2018年 Pacific Asia Museum(パサデナ市、カリフォルニア州) キュレトリアル・コレクション・インターンシップ<br/>
+            　 　展覧会Tsuruya Kōkei: Modern Kabuki Prints Revised & Revisitedの展示デザイン等を担当<br/>
           2020年 国立国際美術館 キュレトリアル・インターンシップ(学芸)<br/>
           <br/>
           受賞・助成抜粋<br/>
